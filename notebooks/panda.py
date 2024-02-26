43c44e264239fe31f7015868c4aaee34622b59a9
okmontie <118560793+montie223@users.noreply.github.com>
2024-02-26 17:32:16 +0000

    commit

---
+++ notebooks/panda.py
@@ -0,0 +1,14 @@
+# %%
+## A playground for me to read datasets and visualization 
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt
+
+# Read the dataset
+df = pd.read_csv('netflix_titles.csv')
+
+x = df['duration']
+
+
+
+# %%
